f2a71c298c7b8a81818f4d1fff014a8b4f84254e
Kirill <kirill@varnakov.com>
2019-07-30 21:25:38 +0300

    Base logging

--- server/__main__.py
+++ server/__main__.py
@@ -2,6 +2,7 @@ import yaml
 from argparse import ArgumentParser
 import socket
 import json
+import logging
 
 from server.protocol import validate_request, make_response
 from server.actions import resolve
@@ -28,16 +29,33 @@ if args.config:
 host, port = config.get('host'), config.get('port')
 buffer_size = config.get('buffer_size')
 
+
+logger = logging.getLogger('main')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+file_handler = logging.FileHandler('main.log')
+stream_handler = logging.StreamHandler()
+
+file_handler.setLevel(logging.DEBUG)
+stream_handler.setLevel(logging.DEBUG)
+
+file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
+
 try:
     sock = socket.socket()
     sock.bind((host, port))
     sock.listen(5)
 
-    print(f'Server started with {host}:{port}')
+    logger.info(f'Server started with {host}:{port}')
 
     while True:
         client, address = sock.accept()
-        print(f'Client was detected {address[0]}:{address[1]}')
+        logger.info(f'Client was detected {address[0]}:{address[1]}')
 
         b_request = client.recv(buffer_size)
         request = json.loads(b_request.decode())
@@ -47,17 +65,17 @@ try:
             controller = resolve(action_name)
             if controller:
                 try:
-                    print('Client sent valid request')
+                    logger.info('Client sent valid request')
                     response = controller(request)
                 except Exception as err:
-                    print(f'Internal server error: {err}')
+                    logger.critical(f'Internal server error: {err}')
                     response = make_response(request, 500, data='Internal server error')
             else:
-                print(f'Controller with action name {action_name} does not exist')
+                logger.error(f'Controller with action name {action_name} does not exist')
                 response = make_response(request, 404, data='Action not found')
         else:
-            print('Client sent wrong request')
-            response = make_response(request, 404, data='Wrong request')
+            logger.error('Client sent wrong request')
+            response = make_response(request, 400, data='Wrong request')
 
         j_response = json.dumps(response)
         client.send(j_response.encode())
