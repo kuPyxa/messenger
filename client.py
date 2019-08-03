96ff5ea0a6fa7ae39bce10becf0cf0a9ac313acb
Kirill <kirill@varnakov.com>
2019-08-03 11:00:12 +0300

    Adding two mode to client

--- client.py
+++ client.py
@@ -5,10 +5,26 @@ import json
 from datetime import datetime
 import zlib
 
+WRITE_MODE = 'write'
+READ_MODE = 'read'
+
+
+def make_request():
+    return {
+        'action': action,
+        'time': datetime.now().timestamp(),
+        'data': message
+    }
+
+
 parser = ArgumentParser()
 
 parser.add_argument(
     '-c', '--config', type=str, required=False, help='Set config file path'
+)
+
+parser.add_argument(
+    '-m', '--mode', type=str, default='write', help='Set client mode'
 )
 
 args = parser.parse_args()
@@ -30,30 +46,22 @@ buffer_size = config.get('buffer_size')
 try:
     sock = socket.socket()
     sock.connect((host, port))
-
     print('Client was started')
 
-    action = input('Enter action: ')
-    message = input('Enter message: ')
+    while True:
+        if args.mode == WRITE_MODE:
+            action = input('Enter action: ')
+            message = input('Enter message: ')
+            request = make_request()
+            j_request = json.dumps(request)
+            bytes_request = zlib.compress(j_request.encode())
+            sock.send(bytes_request)
+            print(f'Client sent request: {request}')
+        elif args.mode == READ_MODE:
+            c_response = sock.recv(buffer_size)
+            b_response = zlib.decompress(c_response)
+            response = json.loads(b_response.decode())
+            print(f'Server sent response {response}')
 
-    request = {
-        'action': action,
-        'time': datetime.now().timestamp(),
-        'message': message
-    }
-
-    j_request = json.dumps(request)
-
-    bytes_request = zlib.compress(j_request.encode())
-
-    sock.send(bytes_request)
-    print(f'Client sent request: {request}')
-
-    c_response = sock.recv(buffer_size)
-
-    b_response = zlib.decompress(c_response)
-
-    response = json.loads(b_response.decode())
-    print(f'Server sent response {response}')
 except KeyboardInterrupt:
     print('Client shutdown')
