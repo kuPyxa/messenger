b0870d46a5098c8599c8d5a7f5b6ad883b14d27c
Kirill <kirill@varnakov.com>
2019-08-07 09:39:41 +0300

    Adding multithreading to server

--- server/__main__.py
+++ server/__main__.py
@@ -3,8 +3,29 @@ from argparse import ArgumentParser
 import socket
 import logging
 import select
+import threading
 
 from handlers import handle_default_request
+
+
+def read(sock, connections, requests, buffer_size):
+    try:
+        b_request = sock.recv(buffer_size)
+    except ConnectionResetError:
+        pass
+    except Exception:
+        connections.remove(read_client)
+    else:
+        requests.append(b_request)
+
+
+def write(sock, connections, b_response):
+    try:
+        sock.send(b_response)
+    except ConnectionResetError:
+        pass
+    except Exception:
+        connections.remove(write_client)
 
 
 parser = ArgumentParser()
@@ -62,20 +83,14 @@ try:
             rlist, wlist, xlist = select.select(connections, connections, connections, 0)
 
             for read_client in rlist:
-                try:
-                    b_request = read_client.recv(buffer_size)
-                except Exception:
-                    connections.remove(read_client)
-                else:
-                    requests.append(b_request)
+                read_thread = threading.Thread(target=read, args=(read_client, connections, requests, buffer_size))
+                read_thread.start()
 
             if requests:
                 b_request = requests.pop()
                 b_response = handle_default_request(b_request)
                 for write_client in wlist:
-                    try:
-                        write_client.send(b_response)
-                    except Exception:
-                        connections.remove(write_client)
+                    write_thread = threading.Thread(target=write, args=(write_client, connections, b_response))
+                    write_thread.start()
 except KeyboardInterrupt:
     print('Server shutdown')
